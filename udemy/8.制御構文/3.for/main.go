85a37ade67848574a3c0e4673e7fe57e058bc779
k_95.21 <kohta0421kochi@gmail.com>
2024-03-06 23:59:59 +0900

    add new codes

--- udemy/8.制御構文/3.for/main.go
+++ udemy/8.制御構文/3.for/main.go
@@ -1,8 +1,33 @@
 package main
 
+import "fmt"
+
 // for
 // 繰り返し処理
 
 func main() {
-	
+	// 無限ループ
+	i := 0
+	for {
+		i ++
+		if i == 3 {
+			break
+		}
+		fmt.Println("Looooop");
+	}
+
+	// 条件付きfor文
+	point := 0
+	for point < 10 {
+		fmt.Println(point)
+		point ++
+	}
+
+	// 古典的for文
+	for i := 0; i < 10; i ++ {
+		if i == 3 {
+			continue
+		}
+		fmt.Println(i)
+	}
 }
