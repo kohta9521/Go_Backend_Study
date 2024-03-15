7fa5da86a5257d7322d5b5bde678fb385c5b033e
k_95.21 <kohta0421kochi@gmail.com>
2024-03-15 11:31:03 +0900

    add original type struct lesson

---
+++ udemy/11.構造体/7.structOriginalType/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// struct
+// original type
+
+type MyInt int
+
+func main() {
+	var m1 MyInt
+	fmt.Println(m1)
+	fmt.Printf("%T\n", m1)
+
+	i := 100
+	fmt.Println(m1 + i)
+
+}
