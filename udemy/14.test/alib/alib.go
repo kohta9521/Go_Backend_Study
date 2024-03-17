fec61561078dde2d29103bd71e269c46f3149a1a
k_95.21 <kohta0421kochi@gmail.com>
2024-03-17 17:09:58 +0900

    add new library

---
+++ udemy/14.test/alib/alib.go
@@ -0,0 +1,8 @@
+package alib
+func Average(s []int) int {
+	total := 0
+	for _, i := range s {
+		total += i
+	}
+	return int(total / len(s))
+}
--- udemy/14.test/main.go
+++ udemy/14.test/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"src/study/Go_Backend_Study/udemy/14.test/alib"
+)
 
 // test
 
@@ -16,4 +19,7 @@ func IsOne(i int) bool {
 func main() {
 	fmt.Println(IsOne(1))
 	fmt.Println(IsOne(0))
+
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(alib.Average(s))
 }
