303d928d37a5aacd275a06848d3c6a2126ffa3fd
k_95.21 <kohta0421kochi@gmail.com>
2024-06-19 11:46:59 +0900

    delete new todo app go lang projesct

---
+++ essence/3.基本的な文法/4.基本的な構文/5.関数呼び出し/main.go
@@ -0,0 +1,17 @@
+package main
+
+// func FindUser(name string) (*User, error) {
+// 	user, err := findUserFromList(users, name)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return user, nil
+// }
+
+func main() {
+	// user, err := FindUser("Bob")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println(user.Name)
+}
--- udemy/18.TODO_App/go.mod
+++
@@ -1,7 +0,0 @@
-module src/study/Go_Backend_Study/udemy/18.TODO_App
-
-go 1.21.3
-
-require gopkg.in/ini.v1 v1.67.0
-
-require gopkg.in/go-ini/ini.v1 v1.67.0 // indirect
